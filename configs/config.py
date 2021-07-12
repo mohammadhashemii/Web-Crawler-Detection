a4215c75b0441af0753d12c3fec99a595052c1b6
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2021-07-12 23:50:47 +0430

    feat: config class added

--- configs/config.py
+++ configs/config.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """configs in json format"""
 
+import json
+
 CFG = {
     "data": {
-        "path": "../dataset/output.log",
+        "path": "./dataset/output.log",
+        "req_threshold": 4,
     },
     "train": {
         "batch_size": 64,
@@ -15,3 +18,23 @@ CFG = {
         "metrics": ["accuracy"]
     }
 }
+
+
+class Config:
+    """Config class which contains data, train and model hyperparameters"""
+
+    def __init__(self, data, train):
+        self.data = data
+        self.train = train
+
+    @classmethod
+    def from_json(cls, cfg):
+        """Creates config from json"""
+        params = json.loads(json.dumps(cfg), object_hook=HelperObject)
+        return cls(params.data, params.train)
+
+
+class HelperObject(object):
+    """Helper class to convert json into Python object"""
+    def __init__(self, dict_):
+        self.__dict__.update(dict_)
