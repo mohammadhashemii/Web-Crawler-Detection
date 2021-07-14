6e04db587694bed0c502370905efb15cf22e2d3e
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2021-07-14 17:54:25 +0430

    fix: more decription added

--- App/frontend/src/components/Description/Elements.js
+++ App/frontend/src/components/Description/Elements.js
@@ -2,13 +2,25 @@ import styled from "styled-components";
 
 
 export const DescriptionContainer = styled.div`
-  height: 100px;
+  height: 60px;
   display: flex;
   justify-content: center;
+  margin-bottom: 60px;
+  display: block;
 `;
 
 export const DescriptionText = styled.p`
     font-size: 15px;
     justify-content: center;
   
+`;
+
+export const SampleLog = styled.code`
+    font-size: 15px;
+    justify-content: center;
+    background-color: #49494a;
+    color: white;
+    padding: 5px;
+    border: transparent;
+    border-radius: 5px;
 `;
--- App/frontend/src/components/Description/index.js
+++ App/frontend/src/components/Description/index.js
@@ -1,11 +1,19 @@
-import {DescriptionContainer, DescriptionText} from "./Elements";
+import {DescriptionContainer, DescriptionText, SampleLog} from "./Elements";
 
 const Description = () => {
     return (
         <>
             <DescriptionContainer>
-                <DescriptionText >This is a demo of the Rahnema College ML internship final project. You can upload a log file which contains
-                    the server logs in order to detect the crawlers of your own website</DescriptionText>
+                <DescriptionText >This is a demo of the <a href="https://rahnemacollege.com/">Rahnema College</a> Machine Learning internship final project. You can upload a log file which contains
+                    the server logs in order to detect the crawlers. <a href="https://github.com/mohammadhashemii/Web-Crawler-Detection">[Source Code]</a>
+                </DescriptionText>
+
+                <DescriptionText>
+                    A sample log is shown here. Therfore, the logs in the file you upload must be the same as the below sample: <a href="https://github.com/mohammadhashemii/Web-Crawler-Detection/blob/master/README.md">[Help]</a>
+                </DescriptionText>
+                <SampleLog>
+                    207.213.193.143 [2021-5-12T5:6:0.0+0430] [Get /cdn/profiles/1026106239] 304 0 [[Googlebot-Image/1.0]] 32
+                </SampleLog>
             </DescriptionContainer>
         </>
     );
